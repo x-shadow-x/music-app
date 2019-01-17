31f6b4279b1c54e929df11e9dd402c8a00c21aff
x <x>
2019-01-17 12:33:18 +0800

    对接歌单数据

--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -11,6 +11,15 @@
             </swiper>
         </div>
         <h2 class="title">热门歌单推荐</h2>
+        <ul>
+            <li v-for="item in discList" :key="item.dissid" class="disc_item">
+                <img :src="item.imgurl" :alt="item.dissname" class="disc_img">
+                <div class="disc_text">
+                    <h2 class="discname" v-html="item.creator.name">{{item.dissname}}</h2>
+                    <p class="desc" v-html="item.dissname"></p>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -23,6 +32,7 @@ export default {
     data() {
         return {
             slider: [],
+            discList: [],
         };
     },
 
@@ -47,7 +57,7 @@ export default {
             try {
                 const res = await getDiscList();
                 if (res.code === SUCC) {
-                    this.slider = res.data.slider;
+                    this.discList = res.data.list;
                 }
             } catch (err) {
                 console.error(err);
@@ -74,4 +84,21 @@ export default {
     text-align center
     color $color-theme
     font-size $font-size-medium
+.disc_item
+    display flex
+    align-items center
+    padding 20px 40px
+.disc_img
+    width 140px
+    height 140px
+    margin-right 40px
+    flex none
+.disc_text
+    flex 1
+    font-size $font-size-medium
+    line-height 1.6
+.desc
+    margin-top 20px
+    font-size $font-size-small
+    color $color-text-d
 </style>
